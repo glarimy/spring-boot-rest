2622bc6e7adc6067e44f9547dc8539be37dad9a6
Krishna Mohan Koyya <krishna@glarimy.com>
2019-02-22 08:41:03 +0530

    updated for CORS

--- glarimy-directory/src/main/java/com/glarimy/directory/rest/EmployeeResource.java
+++ glarimy-directory/src/main/java/com/glarimy/directory/rest/EmployeeResource.java
@@ -3,8 +3,10 @@ package com.glarimy.directory.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.glarimy.directory.api.Directory;
 import com.glarimy.directory.api.Employee;
 
 @RestController
+@CrossOrigin
 public class EmployeeResource {
 	@Autowired
 	private Directory directory;
@@ -23,18 +26,24 @@ public class EmployeeResource {
 	@RequestMapping(value = "/employee", method = RequestMethod.POST)
 	public ResponseEntity<Employee> add(@RequestBody Employee employee) {
 		employee = directory.add(employee);
-		return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		return new ResponseEntity<Employee>(employee, headers, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "/employee/{eid}", method = RequestMethod.GET)
 	public ResponseEntity<Employee> find(@PathVariable("eid") String eid) {
 		Employee employee = directory.find(eid);
-		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		return new ResponseEntity<Employee>(employee, headers,  HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
 	public ResponseEntity<List<Employee>> search(@RequestParam("name") String name) {
 		List<Employee> employees = directory.search(name);
-		return new ResponseEntity<List<Employee>>(employees, HttpStatus.OK);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		return new ResponseEntity<List<Employee>>(employees, headers, HttpStatus.OK);
 	}
 }
